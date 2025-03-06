8753917e9fcd5f768e5345b6a710715b0edbdee8
Sergei Rozhkov <fgup@zserg.ru>
2025-03-06 10:30:05 +0500

    flashforge_init.sh

--- Родная_прошивка/fix_update_mcu/flashforge_init.sh
+++ Родная_прошивка/fix_update_mcu/flashforge_init.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-find /opt/PROGRAM/control/ -name Update -exec rm -f {} \;
-sync
+fix_mcu() 
+{
+    find /opt/PROGRAM/control/ -name Update -exec rm -f {} \;
+    sync
+    find /opt/PROGRAM/control/ -name Update
+    sync
+    rm -f /mnt/flashforge_init.sh
+}
+
+fix_mcu &> /mnt/fix_mcu.log
+
 sleep 5
 reboot
