4813e4a50c5412a0e07e33484d5da270c8245028
Sergei Rozhkov <fgup@zserg.ru>
2025-12-17 17:27:41 +0500

    Update

--- Native_firmware/fix_update_mcu/flashforge_init.sh
+++ Native_firmware/fix_update_mcu/flashforge_init.sh
@@ -2,12 +2,24 @@
 
 fix_mcu()
 {
+    CHECH_ARCH=`uname -m`
+    if [ "${CHECH_ARCH}" == "armv7l" ]; then
+        CONFIG_DIR="/opt/PROGRAM/control/"
+    else if [ "${CHECH_ARCH}" == "mips" ]; then
+        CONFIG_DIR="/usr/prog/PROGRAM/control/"
+    else
+        echo "Machine architecture error."
+        echo ${CHECH_ARCH}
+        exit 1
+    fi
+    fi
+
     echo "Start find 1"
     sync
-    find /opt/PROGRAM/control/ -name Update -exec rm -f {} \;
+    find ${CONFIG_DIR} -name Update -exec rm -f {} \;
     sync
     echo "Start find 2"
-    find /opt/PROGRAM/control/ -name Update
+    find ${CONFIG_DIR} -name Update
     sync
     echo "Rm /mnt/flashforge_init.sh"
     rm -f /mnt/flashforge_init.sh
@@ -17,26 +29,25 @@ fix_mcu()
     sync
     sleep 5
 
-    echo 6 > /sys/class/pwm/pwmchip0/export
-    echo inversed > /sys/class/pwm/pwmchip0/pwm6/polarity
-    echo 400000 > /sys/class/pwm/pwmchip0/pwm6/period
-    echo 200000 > /sys/class/pwm/pwmchip0/pwm6/duty_cycle
-    echo 1 > /sys/class/pwm/pwmchip0/pwm6/enable
-
-    for  i in `seq 1 10`
-    do
-        echo "Beeping cycle..."
-
+    if [ "${CHECH_ARCH}" == "armv7l" ]; then
+        echo 6 > /sys/class/pwm/pwmchip0/export
+        echo inversed > /sys/class/pwm/pwmchip0/pwm6/polarity
         echo 400000 > /sys/class/pwm/pwmchip0/pwm6/period
         echo 200000 > /sys/class/pwm/pwmchip0/pwm6/duty_cycle
-        sleep 0.25
-        echo 800000 > /sys/class/pwm/pwmchip0/pwm6/period
-        echo 400000 > /sys/class/pwm/pwmchip0/pwm6/duty_cycle
-        sleep 0.25
-    done
+        echo 1 > /sys/class/pwm/pwmchip0/pwm6/enable
 
-    reboot
+        for  i in `seq 1 10`; do
+            echo "Beeping cycle..."
+
+            echo 400000 > /sys/class/pwm/pwmchip0/pwm6/period
+            echo 200000 > /sys/class/pwm/pwmchip0/pwm6/duty_cycle
+            sleep 0.25
+            echo 800000 > /sys/class/pwm/pwmchip0/pwm6/period
+            echo 400000 > /sys/class/pwm/pwmchip0/pwm6/duty_cycle
+            sleep 0.25
+        done
+
+    reboot -f
 }
 
 fix_mcu &> /mnt/fix_mcu.log
-
